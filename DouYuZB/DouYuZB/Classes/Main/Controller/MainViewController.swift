ec122178959c44f2d667b255ad39f48c7fcecec7
mario <541496358@qq.com>
2017-11-24 10:01:27 +0800

    update

---
+++ DouYuZB/DouYuZB/Classes/Main/Controller/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  DouYuZB
+//
+//  Created by mario on 2017/11/13.
+//  Copyright © 2017年 mario. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
